26bd14b294f427e1be2c0b811ee2fe085f5f0b10
Talha <talha.satir0729@gmail.com>
2023-05-30 10:19:34 +0300

    fix: update

--- data_table2/lib/main.dart
+++ data_table2/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-//import 'package:data_table_2/data_table_2.dart';
+import 'package:data_table2/data.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -14,34 +14,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(
+          title: 'Paginated DataTable with Sorting Filtering & Pagination'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -49,68 +31,173 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool sort = true;
+  List<Data>? filterData;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  //[MTS] sıralama işlemi için.
+  onsortColum(int columnIndex, bool ascending) {
+    if (columnIndex == 0) {
+      if (ascending) {
+        filterData!.sort((a, b) => a.name!.compareTo(b.name!));
+      } else {
+        filterData!.sort((a, b) => b.name!.compareTo(a.name!));
+      }
+    }
   }
 
   @override
+  void initState() {
+    filterData = myData;
+    super.initState();
+  }
+
+// [MTS] Filter process
+  TextEditingController controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+        ),
+        child: SingleChildScrollView(
+          //[MTS] alttaki sarı yazıyı kaldırmak için.
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                  width: double.infinity,
+                  child: Theme(
+                    data: ThemeData.light()
+                        .copyWith(cardColor: Theme.of(context).canvasColor),
+                    child: PaginatedDataTable(
+                      sortColumnIndex: 0,
+                      sortAscending: sort,
+                      header: Container(
+                        //[MTS] For filter
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey,
+                            ),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: TextField(
+                          //
+                          controller: controller,
+                          decoration: const InputDecoration(
+                              hintText: "Enter something to filter"),
+                          onChanged: (value) {
+                            setState(() {
+                              myData = filterData!
+                                  .where((element) =>
+                                      element.name!.contains(value))
+                                  .toList();
+                            });
+                          },
+                        ),
+                      ),
+                      source: RowSource(
+                        //[MTS] verilerin alındığı yer : galiba
+                        myData: myData,
+                        count: myData.length,
+                      ),
+                      rowsPerPage:
+                          5, // tablonun her bir sayfasında gösterilen veri miktarı
+                      columnSpacing: 8,
+                      columns: [
+                        DataColumn(
+                            label: const Text(
+                              "Name",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600, fontSize: 14),
+                            ),
+                            //[MTS] : shorting processs
+                            onSort: (columnIndex, ascending) {
+                              setState(() {
+                                sort = !sort;
+                              });
+
+                              onsortColum(columnIndex, ascending);
+                            }),
+                        const DataColumn(
+                          label: Text(
+                            "Phone",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 14),
+                          ),
+                        ),
+                        const DataColumn(
+                          label: Text(
+                            "Age",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 14),
+                          ),
+                        ),
+                      ],
+                      showFirstLastButtons:
+                          true, // tablonun altında ilk veya son sayfayı göster seçeneğinin gösterilmesi için.
+                      //showCheckboxColumn: false,
+                      //availableRowsPerPage: [5, 10, 20, 50, 100],
+                      //initialFirstRowIndex: 5,
+                    ),
+                  )),
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                  "Flutter Paginated DataTable With \n\n Sorting \n\n Filtering \n\n Pagination",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w900,
+                      letterSpacing: 5),
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class RowSource extends DataTableSource {
+  var myData;
+  final count;
+  RowSource({
+    required this.myData,
+    required this.count,
+  });
+
+  @override
+  DataRow? getRow(int index) {
+    if (index < rowCount) {
+      return recentFileDataRow(myData![index]);
+    } else
+      return null;
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => count;
+
+  @override
+  int get selectedRowCount => 0;
+}
+
+DataRow recentFileDataRow(var data) {
+  return DataRow(
+    cells: [
+      DataCell(Text(data.name ?? "Name")),
+      DataCell(Text(data.phone.toString())),
+      DataCell(Text(data.Age.toString())),
+    ],
+  );
 }
